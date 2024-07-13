5465f3dce8d07c8ba8bc394521451da62aee7d43
Geani Dumitrache <geani.g.dumitrache@gmail.com>
2024-07-13 18:23:20 +0300

    Add environment variable replacement in build process
    A new script has been created to replace environment variables in `auth_config.json` during the build process. This script is invoked in `package.json` build scripts, enabling the application to utilize environment specific configurations. Dotenv file has been added to store environment variables locally for the development environment.

---
+++ replace-env.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+function replaceEnvVars() {
+  const envVars = [
+    'AUTH0_DOMAIN',
+    'AUTH0_CLIENT_ID',
+    'AUTH0_AUDIENCE',
+    'API_URI',
+    'APP_URI'
+  ];
+
+  let config = fs.readFileSync('auth_config.json', 'utf-8');
+
+  envVars.forEach((key) => {
+    const value = process.env[key];
+    if (value) {
+      const regex = new RegExp(`\\$\\{${key}\\}`, 'g');
+      config = config.replace(regex, value);
+    } else {
+      console.error(`Missing environment variable: ${key}`);
+    }
+  });
+
+  fs.writeFileSync('auth_config.json', config);
+}
+
+replaceEnvVars();
